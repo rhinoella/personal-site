19fae671e59ecadf986985426067f78f4d7e782f
Noella Spitz <noella.spitz@gmail.com>
2024-02-21 12:58:59 +0000

    Remove contact

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Hero } from "./components/Hero";
 import { Skills } from "./components/Skills";
 import { Experience } from "./components/Experience";
 import { About } from "./components/About";
-import { Contact } from "./components/Contact";
 
 function App() {
   return (
